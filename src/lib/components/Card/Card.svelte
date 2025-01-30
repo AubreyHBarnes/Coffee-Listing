4842a003373a632d204c14c9bae9bbc5dc6cd98f
AubreyHBarnes <aubrey.hbarnes@gmail.com>
2025-01-30 09:28:35 -0600

    styling and formatting data WIP

--- src/lib/components/Card/Card.svelte
+++ src/lib/components/Card/Card.svelte
@@ -1,8 +1,22 @@
 <script>
-    let {coffeeName, coffeeImg} = $props();
+    let {coffeeName, coffeeImg,  coffeePopular, coffeePrice, coffeeRating, coffeeVotes} = $props();
 </script>
 
-<div>
-    <img alt="" src="{coffeeImg}" />
-    <p>{coffeeName}</p>
+<div class="cardWrapper">
+    <div class="cardImgWrapper">
+        {#if coffeePopular === true}
+            <p class="absolute mx-2 my-2 px-2 py-1 bg-yellow-400 rounded-xl text-black text-xs font-medium">Popular</p>
+        {/if}
+        <img class="rounded-xl w-full" alt="" src="{coffeeImg}" />
+    </div>
+    <div class="cardPriceWrapper flex justify-between pt-2">
+        <p class="text-white font-medium">{coffeeName}</p>
+        <span class="text-black bg-[#FEF7EE] rounded-md px-2 py-1 font-medium text-sm h-min">{coffeePrice}</span>
+    </div>
+    <div class="cardRatingWrapper">
+        <img alt="" src="" />
+        {#if coffeeVotes}
+            <p>ratings</p>
+        {/if}
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,28 @@
 <script>
     import Card from '$lib/components/Card/Card.svelte';
     let {data} = $props();
+    console.log(data.item)
     
 </script>
+<img alt="" src="/img/bg-cafe-lg.jpg" />
+<div class="floatingContainer px-4 py-8 md:p-[3rem] md:mx-[6rem] mb-[3rem] bg-[#111315] relative bottom-3 md:bottom-8 rounded-2xl text-white">
+    <div class="headingCopyContainer m-auto max-w-xl text-center">
+        <h1 class="text-4xl font-medium">Our Collection</h1>
+        <p>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins,
+             expertly roasted in small batches and shipped fresh&nbsp;weekly.</p>
+    </div>
+    <div class="CardGrid w-full max-md:mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {#each data.item
+            as { name, image, popular, price, rating, votes }}
 
-
-
-<h1>Our Collection</h1>
-<p>this is the home page.</p>
-
-{#each data.item
-    as { name, image }}
-
-    <Card
-        coffeeName={name}
-        coffeeImg={image}
-    />
-{/each}
+            <Card
+                coffeeName={name}
+                coffeeImg={image}
+                coffeePopular={popular}
+                coffeePrice={price}
+                coffeeRating={rating}
+                coffeeVotes={votes}
+            />
+        {/each}
+    </div>
+</div>
