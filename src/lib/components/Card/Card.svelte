cd7ba181622ad81db94dee7f09c638f28ff5ca3a
AubreyHBarnes <aubrey.hbarnes@gmail.com>
2025-01-29 18:09:34 -0600

    Created Card Component

---
+++ src/lib/components/Card/Card.svelte
@@ -0,0 +1,8 @@
+<script>
+    let {coffeeName, coffeeImg} = $props();
+</script>
+
+<div>
+    <img alt="" src="{coffeeImg}" />
+    <p>{coffeeName}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 
 <script>
+    import Card from '$lib/components/Card/Card.svelte';
     let {data} = $props();
-    console.log(data.item)
+    
 </script>
+
+
 
 <h1>Our Collection</h1>
 <p>this is the home page.</p>
 
 {#each data.item
     as { name, image }}
-<p>{name}</p>
-<img alt="" src="{image}" />
 
+    <Card
+        coffeeName={name}
+        coffeeImg={image}
+    />
 {/each}
